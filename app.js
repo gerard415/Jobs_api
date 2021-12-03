037c79994c93aadf6ad8449e18516c0f08b21d2e
Chinedum <chinedumike85@gmail.com>
2021-12-03 16:47:25 +0100

    setting up the first set of routes and controllers, and creating  a User model

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 require('dotenv').config();
 require('express-async-errors');
+
 const express = require('express');
-const app = express();
+const app = express()
+
+const connectDB = require('./db/connect')
+
+//routes
+const authRoute = require('./routes/auth')
+const jobsRoute = require('./routes/jobs')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
@@ -11,19 +18,21 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/jobs', jobsRoute)
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+
+
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
     console.log(error);
---
+++ routes/jobs.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express()
+const {getAlljobs, getJob, createJob, updateJob, deleteJob} = require('../controllers/jobs')
+
+router.route('/').get(getAlljobs).post(createJob)
+router.route('/:id').get(getJob).patch(updateJob).delete(deleteJob)
+
+module.exports = router 
